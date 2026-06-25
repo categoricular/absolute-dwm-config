01d0acd1a7dfd34d14ed88338d88d46e5d409ee7
Categoricular <4volacno@gmail.com>
2026-06-25 20:36:04 +0200

    borders + increased gap sizes

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx = 1; /* border pixel of windows */
+static const unsigned int borderpx = 3; /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
 static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
@@ -9,7 +9,7 @@ static const char *fonts[] = {"BmapNF:size=11:antialias=true:autohint=true"};
 static const char dmenufont[] = "BmapNF:size=11";
 
 /* gap size */
-static const unsigned int gappx = 12;
+static const unsigned int gappx = 14;
 
 /* Catpuccing Mocha Color Scheme */
 static const char cpp_base[] = "#1e1e2e";    // background
