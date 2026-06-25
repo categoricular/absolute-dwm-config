af2edf37f2a754ea182d0943ff0052499ec376ac
Categoricular <4volacno@gmail.com>
2026-06-25 20:21:55 +0200

    changed font + icons

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx = 1; /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
 static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
-static const char *fonts[] = {"monospace:size=10"};
-static const char dmenufont[] = "monospace:size=10";
+static const char *fonts[] = {"BmapNF:size=11:antialias=true:autohint=true"};
+static const char dmenufont[] = "BmapNF:size=11";
 
 /* gap size */
 static const unsigned int gappx = 12;
@@ -25,7 +25,7 @@ static const char *colors[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+static const char *tags[] = {"", "", "󰅩", "󰙯", "󰝚", "󱔗"};
 
 static const Rule rules[] = {
     /* xprop(1):
