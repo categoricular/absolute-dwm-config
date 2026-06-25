1fe9952b55150b1398224c034dda3b02faf94f98
Categoricular <4volacno@gmail.com>
2026-06-25 21:34:27 +0200

    added padding inside of the bar

--- config.h
+++ config.h
@@ -1,10 +1,11 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx = 3; /* border pixel of windows */
-static const unsigned int snap = 32;    /* snap pixel */
-static const int showbar = 1;           /* 0 means no bar */
-static const int topbar = 1;            /* 0 means bottom bar */
+static const unsigned int borderpx = 3;   /* border pixel of windows */
+static const unsigned int barpadding = 8; /* vertical inside the bar */
+static const unsigned int snap = 32;      /* snap pixel */
+static const int showbar = 1;             /* 0 means no bar */
+static const int topbar = 1;              /* 0 means bottom bar */
 static const char *fonts[] = {"BmapNF:size=11:antialias=true:autohint=true"};
 static const char dmenufont[] = "BmapNF:size=11";
 
